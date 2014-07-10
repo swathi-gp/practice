b05b6910592bc9d06ebaafb914d7ca8d1d176ac0
Swathi <swathi@printgreener.com>
2014-07-10 18:44:25 +0530

    practice repository

---
+++ jq.php
@@ -0,0 +1,9 @@
+<form action="return.php" class="js-ajax-php-json" method="post" accept-charset="utf-8">
+<input type="text" name="favorite_beverage" value="" placeholder="Favorite restaurant" />
+<input type="text" name="favorite_restaurant" value="" placeholder="Favorite beverage" />
+<select name="gender">
+<option value="male">Male</option>
+<option value="female">Female</option>
+</select>
+<input type="submit" name="submit" value="Submit form" />
+</form>
