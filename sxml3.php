61dd72287fe0b6eb7f1e6d205d00bd7580c57c83
Swathi <swathi@printgreener.com>
2014-07-16 13:52:23 +0530

    It is my practice repository for json

--- sxml3.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'sxml1.php';
-$movies=new SimpleXMLElement($xmlstr);
-foreach($movies->xpath('//character') as $character)
-{
-echo $character->name, 'placed by',$character->actor;
-}
-?>
